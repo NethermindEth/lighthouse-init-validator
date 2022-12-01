58ec1ba2ea2569b2e838321aa8584092b2e2e3cf
adriantpaez <adriantpaez@gmail.com>
2022-12-01 11:54:04 -0500

    feat: add slashing data file
    slashing data file is used to import slashing data protection following
    the EIP-3076 specification

--- validator-init.sh
+++ validator-init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+SLASHING_DATA_FILE=$1
+
 for key in /keystore/validator_keys/*; do
   if [ -f "$key" ]; then
     echo "Found validator key in $key"
@@ -9,3 +11,10 @@ for key in /keystore/validator_keys/*; do
     echo "Validator imported"
   fi
 done
+
+if [ -f "$SLASHING_DATA_FILE" ]; then
+  echo "Found slashing interchange data in $SLASHING_DATA_FILE"
+  echo "Importing slashing data..."
+  lighthouse account validator slashing-protection import $SLASHING_DATA_FILE
+  echo "Slashing data imported"
+fi
